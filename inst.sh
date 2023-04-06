8209601178440a69ce5dc8c082ffc01b0b40010c
Francis ABODO <francis.abodo@solvr.fr>
2023-04-06 13:34:24 +0100

    Removing inst.sh file

--- inst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo chmod +x *.sh
-./Docker-script.sh
-sleep 5
-./Docker-compose-script.sh
-sleep 5
-./Jenkins-scritp.sh
-#
-echo "--------------------- YOU CAN USE JENKINS ------------------------"
