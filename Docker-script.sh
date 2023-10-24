739d8bbf02c5a786518c358829d00890b56011cb
AhilleUlrich <98032824+AhilleUlrich@users.noreply.github.com>
2023-10-24 04:18:06 +0100

    Update Docker-script.sh

--- Docker-script.sh
+++ Docker-script.sh
@@ -3,7 +3,7 @@
 sudo apt update -y
 sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt install docker-ce docker-ce-cli containerd.io -y
 sudo usermod -aG docker $USER
 newgrp docker
